c05d10b2a87c1265f91932ba5a8c6cea0acf71cc
Logistica <Logistica@LOGISTICA-PC>
2019-07-20 20:40:55 -0600

    Creando Archibo DB

---
+++ QuieroPizza/QuieroPizza.BL/ProductosBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+    public class ProductosBL
+    {
+        Contexto _contexto;
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+        }
+        public List<Producto> ObtenerProductos()
+        {
+            
+        
+            return _contexto.Productos.ToList();
+        }
+    }
+}
---
+++ QuieroPizza/QuieroPizza.Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using QuieroPizza.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+
+            foreach (var producto in listadeProductos)
+            {
+                Console.WriteLine(producto.Id +" "+ producto.Descripcion +" "+ producto.Precio);
+                                                       
+            }
+
+            Console.ReadLine (); 
+        }
+    }
+}
